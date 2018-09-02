3f505d328e04b7f6cc9368fe5bfa67e5a68ff967
Shivam Kapoor <mailtoshivamk@gmail.com>
2018-09-03 00:58:13 +0530

    Update dropbox-whitelist-selective-sync.sh

--- dropbox-whitelist-selective-sync.sh
+++ dropbox-whitelist-selective-sync.sh
@@ -3,7 +3,7 @@
 LOGFILE="/var/log/dropbox-whitelist.log"
 
 function sighuphandler() {
-    exec > >(tee "$LOGFILE") 2>&1
+	exec > >(tee "$LOGFILE") 2>&1
 }
 
 trap sighuphandler SIGHUP
@@ -14,18 +14,18 @@ DROPBOX_SYNC_DIR="$1"
 DROPBOX_WHITELIST_FILE="$DROPBOX_SYNC_DIR/.dropbox-whitelist"
 
 if [ -z "$DROPBOX_SYNC_DIR" ]; then
-    echo "[`date "+%Y-%m-%d %H:%M:%S"`] [ERROR] - No arguments specified! Please provide a correct dropbox sync directory as an argument to this script."
+	echo "[`date "+%Y-%m-%d %H:%M:%S"`] [ERROR] - No arguments specified! Please provide a correct dropbox sync directory as an argument to this script."
 	exit 1
 fi
 
 if [ ! -d $DROPBOX_SYNC_DIR ]; then
-    echo "[`date "+%Y-%m-%d %H:%M:%S"`] [ERROR] - Directory $DROPBOX_SYNC_DIR doesn't exists! Please provide a correct dropbox sync directory."
-    exit 1
+	echo "[`date "+%Y-%m-%d %H:%M:%S"`] [ERROR] - Directory $DROPBOX_SYNC_DIR doesn't exists! Please provide a correct dropbox sync directory."
+	exit 1
 fi
 
 if [ ! -f $DROPBOX_WHITELIST_FILE ]; then
-    echo "[`date "+%Y-%m-%d %H:%M:%S"`] [ERROR] - File $DROPBOX_WHITELIST_FILE not found!"
-    exit 1
+	echo "[`date "+%Y-%m-%d %H:%M:%S"`] [ERROR] - File $DROPBOX_WHITELIST_FILE not found!"
+	exit 1
 fi
 
 LOGPATH="/var/log"
@@ -50,7 +50,7 @@ while :; do
 	# List of all the files and directories under $DROPBOX_SYNC_DIR.
 	read -ra dl <<< "`find $DROPBOX_SYNC_DIR \( ! -regex '.*/\..*' \) | awk '{if(NR>1)print}' | cut -c $(expr $(echo $DROPBOX_SYNC_DIR | wc -c) + 1)- | paste -d\| -s`"
 
-    echo "[`date "+%Y-%m-%d %H:%M:%S"`] [DEBUG] - List of all the files and directories under $DROPBOX_SYNC_DIR: ${dl[*]}"
+	echo "[`date "+%Y-%m-%d %H:%M:%S"`] [DEBUG] - List of all the files and directories under $DROPBOX_SYNC_DIR: ${dl[*]}"
 
 	# List of all the whitelisted files and directories under $DROPBOX_SYNC_DIR.
 	# Validation of file and directory names can't be performed for whitelisting based selective sync approach. Hence it is assumed that the file and directoriy names mentioned in whitelist are indeed correct. Incorrect file and directory names may result in undesirable results.
@@ -59,20 +59,20 @@ while :; do
 		wl+=("$i")
 	done < $DROPBOX_WHITELIST_FILE
 
-    echo "[`date "+%Y-%m-%d %H:%M:%S"`] [DEBUG] - List of all the whitelisted files and directories under $DROPBOX_SYNC_DIR: ${wl[*]}"
+	echo "[`date "+%Y-%m-%d %H:%M:%S"`] [DEBUG] - List of all the whitelisted files and directories under $DROPBOX_SYNC_DIR: ${wl[*]}"
 
 	# List of elements from Directorylist that don't start with any element from Whitelist.
 	diff1=()
 	for i in "${dl[@]}"; do
 		flag=0
 		for j in "${wl[@]}"; do
-		        if [[ "$i" == "$j"* ]]; then
-		    flag=1
-		    break
-		        fi
+			if [[ "$i" == "$j"* ]]; then
+				flag=1
+				break
+			fi
 		done
 		if [ $flag == 0 ]; then
-		        diff1+=("$i")
+			diff1+=("$i")
 		fi
 	done
 
@@ -82,8 +82,8 @@ while :; do
 		flag=0
 		for j in "${wl[@]}"; do 
 			if [[ "$j" == "$i"* ]]; then
-		        flag=1
-		        break
+				flag=1
+				break
 			fi
 		done 
 		if [ $flag == 0 ]; then
@@ -96,13 +96,13 @@ while :; do
 	for i in "${diff2[@]}"; do
 		flag=0
 		for j in "${bl[@]}"; do
-		    if [[ "$i" == "$j"* ]]; then
-		        flag=1
-		        break
-		    fi
+			if [[ "$i" == "$j"* ]]; then
+				flag=1
+				break
+			fi
 		done
 		if [ $flag == 0 ]; then
-		    bl+=("$i")
+			bl+=("$i")
 		fi
 	done
 
@@ -110,17 +110,17 @@ while :; do
 
 	# Reset dropbox exclusion list everytime .dropbox-whitelist is updated. 
 	# Since sync after reset may take some time for files of bigger sizes, it may take a couple of iterations before they get included or excluded as part of dropbox exclusion list. This is one particular reason why file and directory names can't be validated locally per iteration.
-    md5=`md5sum $DROPBOX_WHITELIST_FILE | cut -d" " -f 1`
-    if [ "$md5" != "$_md5" ]; then
+	md5=`md5sum $DROPBOX_WHITELIST_FILE | cut -d" " -f 1`
+	if [ "$md5" != "$_md5" ]; then
 		echo "[`date "+%Y-%m-%d %H:%M:%S"`] [INFO] - Resetting dropbox exclusion list since $DROPBOX_WHITELIST_FILE is modified! New md5 $md5 vs old md5 $_md5."
 
-        _md5=$md5
-        dropbox exclude remove $DROPBOX_SYNC_DIR
-    fi
+		_md5=$md5
+		dropbox exclude remove $DROPBOX_SYNC_DIR
+	fi
 
 	echo "[`date "+%Y-%m-%d %H:%M:%S"`] [INFO] - Updating dropbox exclusion list."
 	for i in "${bl[@]}"; do dropbox exclude add "$DROPBOX_SYNC_DIR/$i"; done
-
+	
 	rotateLog &	
 
 	echo "[`date "+%Y-%m-%d %H:%M:%S"`] [INFO] - Sleep for 10 sec..."
